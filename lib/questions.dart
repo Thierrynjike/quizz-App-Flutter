abcd03433f11f70b0f8af1d51a33710ef196577e
NoNameNigger <thierrynjike14@gmail.com>
2020-04-15 06:41:02 +0200

    quizz Page

--- lib/questions.dart
+++ lib/questions.dart
@@ -31,12 +31,12 @@ class Question {
     new Question('La lune va finir par tomber sur terre à cause de la gravité', false, 'Au contraire la lune s\'éloigne', 'quizz assets/lune.jpg'),
     new Question('La Russie est plus grande en superficie que Pluton', true, '', 'quizz assets/russie.jpg'),
     new Question('Nyctalope est une race naine d\'antilope', false, 'C’est une aptitude à voir dans le noir', 'quizz assets/nyctalope.jpg'),
-    new Question('Le Commodore 64 est l\’oridnateur de bureau le plus vendu', true, '', 'quizz assets/commodore.jpg'),
-    new Question('Le nom du drapeau des pirates es black skull', false, 'Il est appelé Jolly Roger', 'quizz assets/pirate.png'),
+    new Question('Le Commodore 64 est l\’ordinateur de bureau le plus vendu', true, '', 'quizz assets/commodore.jpg'),
+    new Question('Le nom du drapeau des pirates est black skull', false, 'Il est appelé Jolly Roger', 'quizz assets/pirate.png'),
     new Question('Haddock est le nom du chien Tintin', false, 'C\'est Milou', 'quizz assets/tintin.jpg'),
     new Question('La barbe des pharaons était fausse', true, 'A l\'époque déjà ils utilisaient des postiches', 'quizz assets/pharaon.jpg'),
-    new Question('Au Québec tire toi une bûche veut dire viens viens t\'asseoir', true, 'La bûche, fameuse chaise de bucheron', 'quizz assets/buche.jpg'),
-    new Question('Le module lunaire Eagle de possédait de 4Ko de Ram', true, 'Dire que je me plains avec mes 8GO de ram sur mon mac', 'quizz assets/eagle.jpg'),
+    new Question('Au Québec tire toi une bûche veut dire viens t\'asseoir', true, 'La bûche, fameuse chaise de bucheron', 'quizz assets/buche.jpg'),
+    new Question('Le module lunaire Eagle possédait de 4Ko de Ram', true, 'Dire que je me plains avec mes 8GO de ram sur mon pc', 'quizz assets/eagle.jpg'),
   ];
 
   int index=0;
@@ -63,7 +63,7 @@ class Question {
                 color: Colors.white,
               ),
             ),
-            new Text('Scrore $score / $total',
+            new Text('Score :  $score / $total',
               style: new TextStyle(
                 color: Colors.white,
               ),
@@ -105,7 +105,7 @@ class Question {
 
                 ),
                 new RaisedButton(
-                    onPressed: null,
+                    onPressed: faux,
                     color: Colors.grey[900],
                     child: new Text('Faux',
                       style: new TextStyle(
@@ -183,10 +183,11 @@ class Question {
                       onPressed: () {
                         setState(() {
                           (index==listeQuestions.length) ? index=0 : index++;
+                          numq++;
 
                           Navigator.pop(context);
-                          (index!=listeQuestions.length) ? new Questions() : Navigator.popUntil(context, ModalRoute.withName('/'));
-                          numq = index+1;
+                          (numq==11) ? Navigator.popUntil(context, ModalRoute.withName('/')) : new Questions() ;
+
                         });
                       },
                       child: new Text((index==listeQuestions.length-1) ? 'Terminé': 'Suivant')),
@@ -197,6 +198,76 @@ class Question {
         );
       }
     }
+
+
+  Future<Null> faux() async {
+    if(!listeQuestions[index].reponse){
+      return showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return new AlertDialog(
+              title: new Text('Congratulations',
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  color: Colors.green,
+
+                ),
+              ),
+              contentPadding: EdgeInsets.only(left:2.0,right:2.0,bottom: 2.0),
+              content: new Image.asset('quizz assets/vrai.jpg'),
+              actions: <Widget>[
+                new FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        (index==listeQuestions.length-1) ? index=0 : index++;
+                        score++;
+                        numq++;
+                        Navigator.pop(context);
+                        (numq==11) ? Navigator.popUntil(context, ModalRoute.withName('/')) : new Questions();
+                      });
+                    },
+                    child: new Text((index==listeQuestions.length-1) ? 'Terminé': 'Suivant')),
+
+              ],
+            );
+          }
+      );
+    }
+    else{
+      return showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return new AlertDialog(
+              title: new Text('Wrong Answer',
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  color: Colors.red,
+
+                ),
+              ),
+              contentPadding: EdgeInsets.only(left:2.0,right:2.0,bottom: 2.0),
+              content: new Image.asset('quizz assets/faux.jpg'),
+              actions: <Widget>[
+                new FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        (index==listeQuestions.length-1) ? index=0 : index++;
+                        numq++;
+                        Navigator.pop(context);
+                        (numq==11) ? Navigator.popUntil(context, ModalRoute.withName('/')) : new Questions();
+
+                      });
+                    },
+                    child: new Text((index==listeQuestions.length-1) ? 'Terminé': 'Suivant')),
+
+              ],
+            );
+          }
+      );
+    }
+  }
 
   }
 
